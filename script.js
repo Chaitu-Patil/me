610197bae51b24dd2aa2aece0612ed9b22fd6571
Chaitu-Patil <chaitanyahpatil@gmail.com>
2025-12-29 11:55:02 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+    
+
+const fadeElements = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target); // animate once
+      }
+    });
+  },
+  {
+    threshold: 0.2
+  }
+);
+
+fadeElements.forEach(el => observer.observe(el));
+
+
+
+
+
+const progressBar = document.getElementById("progress-bar");
+
+window.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+
+  progressBar.style.width = scrollPercent + "%";
+});
